f8e8e3ec1ca28b871261ebdabc1be414e3416ed7
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-11-17 15:18:04 +0100

    code de Grid sans la méthode shoot()

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -2,27 +2,41 @@ package model;
 
 import model.weapons.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Grid {
     private int _width;
     private int _height;
+    private List<Placeable> _placedObjects;
 
     public Grid(int width, int height){
         _width = width;
         _height = height;
+        _placedObjects = new ArrayList<>();
     }
 
     public Boolean isOccupied(Cell cell){
+        for (Placeable p : _placedObjects){
+            for (Cell c : p.getCells()){
+                if (c.equals(cell)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public Boolean isHit(Cell cell){
+        return cell.isHit();
     }
 
     public void placeObject(Placeable placeable){
-
+        _placedObjects.add(placeable);
     }
 
     public void placeImpact(Cell cell){
-
+        _placedObjects.add(new Impact(cell));
     }
 
     public void shoot(Cell cell, Weapon weapon){

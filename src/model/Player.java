0a58509b74eff71c7c0914b8e012969d437ee4c5
Bastien <bastien.isnard@etu-unistra.fr>
2025-11-17 15:10:15 +0100

    Player implementé

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,27 @@
 package model;
 
+import model.weapons.Weapon;
+
 public class Player {
+    private String _name;
+    private Grid _grid;
+
+    public Player(String name) {
+        _name = name;
+    }
+
+    public String getName() {
+        return _name;
+    }
+    public Grid getGrid() {
+        return _grid;
+    }
+
+    public void placeShip(Ship ship) {
+        _grid.placeObject(ship);
+    }
+
+    public void shoot(Cell cell, Weapon weapon) {
+        Grid.shoot(cell,weapon);
+    }
 }

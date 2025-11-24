ed48f135c11014b034cffec1ff8c106ef76d71ad
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-11-24 16:57:11 +0100

    début de code de Game (il manque des méthodes dans grid)

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,4 +1,21 @@
 package model;
 
 public class Game {
+    private int _turnNum;
+
+    private Player[] _player;
+
+    public Game() {}
+
+    public void setPlayerName(String name){
+
+    }
+
+    public void setGrid(int width, int height, boolean islandMode) {
+        Grid grid = new Grid(width, height);
+    }
+
+    public Boolean isTheGameFinished(){
+        return false;
+    }
 }

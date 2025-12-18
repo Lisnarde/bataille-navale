ad01e67c02477ca14945e49924764552ed7e4959
ISNARD BASTIEN <bastien.isnard@etu.unistra.fr>
2025-12-18 18:34:20 +0100

    2e ajout du précédent commit

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -3,6 +3,10 @@ package controller;
 import controller.bots.Bot;
 import model.*;
 import model.traps.Trap;
+import model.weapons.Bomb;
+import model.weapons.Sonar;
+import model.weapons.Weapon;
+import model.weapons.WeaponTypes;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -52,7 +56,14 @@ public class GameController {
         return _model.placeShipOnGrid(joueur,ship);
     }
 
-    public boolean placeWeaponOnIsland()
+    public boolean placeWeaponOnIsland(int joueur, WeaponTypes type, int posx, int posy) {
+        Weapon weapon = switch (type) {
+            case BOMB -> new Bomb();
+            case SONAR -> new Sonar();
+            default -> null;
+        };
+        return _model.placeWeaponOnIsland(joueur, weapon, new Cell(posx,posy));
+    }
 
     public boolean setWeapon(int joueur, int weaponIndex) {
         return _model.setWeapon(joueur,weaponIndex);

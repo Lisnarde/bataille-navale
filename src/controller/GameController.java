48943bba12c1e1a671e05df807a6e3bf47a7d972
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-12-19 00:10:31 +0100

    adaptation de l'uml du controller + rename de la variable joueur en player

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -42,45 +42,45 @@ public class GameController {
         return false;
     }
 
-    public void botPlaceObjects(int joueur) {
-        _bot.placeShips(joueur,_model,this, _model.getNumberPerShip());
+    public void botPlaceObjects(int player) {
+        _bot.placeShips(player,_model,this, _model.getNumberPerShip());
     }
 
-    public boolean placeTrapOnGrid(int joueur, int indexTrap, int posx, int posy) {
+    public boolean placeTrapOnGrid(int player, int indexTrap, int posx, int posy) {
         Trap trap = switch (_model.getTrapInInventory(indexTrap)) {
             case BLACKHOLE -> new BlackHole();
             case TORNADO -> new Tornado();
         };
         trap.setPosition(new Cell(posx, posy));
-        return _model.placeTrapOnGrid(joueur, trap);
+        return _model.placeTrapOnGrid(player, trap);
     }
 
-    public boolean placeShipOnGrid(int joueur, ShipTypes type, int posx, int posy, Axis axis) {
+    public boolean placeShipOnGrid(int player, ShipTypes type, int posx, int posy, Axis axis) {
         int size = type.getSize();
         List<Cell> position = new ArrayList<>();
         for (int i=0; i<size; i++) {
             position.add( new Cell(posx + (axis == Axis.HORIZONTAL ? i : 0) , posy + (axis == Axis.VERTICAL ? i : 0)));
         }
         Ship ship = new Ship(type,position);
-        return _model.placeShipOnGrid(joueur,ship);
+        return _model.placeShipOnGrid(player,ship);
     }
 
-    public boolean placeWeaponOnIsland(int joueur, int weaponIndex, int posx, int posy) {
+    public boolean placeWeaponOnIsland(int player, int weaponIndex, int posx, int posy) {
         Weapon weapon = switch (_model.getWeaponInGlobalInventory(weaponIndex)) {
             case BOMB -> new Bomb();
             case SONAR -> new Sonar();
             default -> null;
         };
-        return _model.placeWeaponOnIsland(joueur, weapon, new Cell(posx,posy));
+        return _model.placeWeaponOnIsland(player, weapon, new Cell(posx,posy));
     }
 
-    public boolean setWeapon(int joueur, int weaponIndex) {
-        return _model.setWeapon(joueur,weaponIndex);
+    public boolean setWeapon(int player, int weaponIndex) {
+        return _model.setWeapon(player,weaponIndex);
     }
 
-    public boolean shootOnGrid(int joueur, int posx, int posy) {
-        if (_model.shootOnGrid(joueur, new Cell(posx, posy))) {
-            if (joueur==0) {
+    public boolean shootOnGrid(int player, int posx, int posy) {
+        if (_model.shootOnGrid(player, new Cell(posx, posy))) {
+            if (player==0) {
                 _bot.shoot(1,_model, this);
             }
             return true;
@@ -88,7 +88,7 @@ public class GameController {
         return false;
     }
 
-    public void washGrid(int joueur) {
-        _model.washGrid(joueur);
+    public void washGrid(int player) {
+        _model.washGrid(player);
     }
 }

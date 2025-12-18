79681490c9af3bad9ef4cd0d63afc302fc38aff2
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-12-18 18:37:10 +0100

    Configuration du nombre de bateaux par type (graphique et logique)

--- src/view/themes/Theme.java
+++ src/view/themes/Theme.java
@@ -6,6 +6,9 @@ import java.awt.*;
 public interface Theme {
     Font normalFont();
     Font boldFont();
+    Font titleFont();
 
     void buttonTheme(AbstractButton button);
+    void buttonConfigMinusTheme(AbstractButton button);
+    void buttonConfigPlusTheme(AbstractButton button);
 }

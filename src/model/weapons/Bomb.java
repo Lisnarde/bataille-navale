29e9a292856ba074bda5ef11458fe4ac3fc1466a
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-12-18 11:04:05 +0100

    changement du fonctionnement de bomb car erreur de compréhension au début

--- src/model/weapons/Bomb.java
+++ src/model/weapons/Bomb.java
@@ -9,6 +9,17 @@ public class Bomb extends model.weapons.Weapon{
     @Override
     public boolean execShoot(Grid grid, Cell cell) {
         int hitCells = 0;
+
+        hitCells += applyImpact(grid, cell);
+        hitCells += applyImpact(grid, new Cell(cell.getX(), cell.getY() - 1));
+        hitCells += applyImpact(grid, new Cell(cell.getX(), cell.getY() + 1));
+        hitCells += applyImpact(grid, new Cell(cell.getX() - 1, cell.getY()));
+        hitCells += applyImpact(grid, new Cell(cell.getX() + 1, cell.getY()));
+
+        return hitCells > 0;
+
+
+        /*
         for (int x=cell.getX()-1; x<cell.getX()+2; x++) {
             for (int y=cell.getY()-1; y<cell.getY()+2; y++) {
                 Cell newCell = new Cell(x,y);
@@ -24,7 +35,27 @@ public class Bomb extends model.weapons.Weapon{
             }
         }
         return hitCells > 0;
+        */
     }
+
+    private int applyImpact(Grid grid, Cell c) {
+        if (!grid.isInGrid(c) || grid.isOccupiedBy(c, PlaceableTypes.ISLANDPART)) {
+            return 0;
+        }
+
+        grid.placeImpact(c);
+
+        boolean hit = grid.isOccupiedBy(c, PlaceableTypes.SHIP);
+        grid.notifyObserversShoot(c.getX(), c.getY(), hit);
+
+        if (grid.isOccupiedBy(c, PlaceableTypes.TRAP)) {
+            Trap trap = (Trap) grid.getObjectByPosition(c);
+            grid.notifyObserversTrapActivated(c.getX(), c.getY(), trap.getTrapType());
+        }
+
+        return 1;
+    }
+
 
     @Override
     public WeaponTypes getWeaponType() {

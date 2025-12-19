2e70a0d2625583da77a554f7147ca1eab8051a31
Noah Dumangin <noah.dumangin@etu.unistra.fr>
2025-12-19 13:51:47 +0100

    uml du model fini

--- src/model/GridObserver.java
+++ src/model/GridObserver.java
@@ -4,19 +4,19 @@ import model.traps.TrapTypes;
 import model.weapons.WeaponTypes;
 
 public interface GridObserver {
-    void updateShoot(int joueur, int posx, int posy, boolean hit);
+    void updateShoot(int player, int posx, int posy, boolean hit);
 
-    void updateTrapActivated(int joueur, int posx, int posy, TrapTypes trapType);
+    void updateTrapActivated(int player, int posx, int posy, TrapTypes trapType);
 
-    void updateSearch(int joueur, int posx, int posy, WeaponTypes objectFound);
+    void updateSearch(int player, int posx, int posy, WeaponTypes objectFound);
 
-    void updateShipCellPlaced(int joueur, int posx, int posy);
+    void updateShipCellPlaced(int player, int posx, int posy);
 
-    void updateTrapPlaced(int joueur, int posx, int posy, TrapTypes trapType);
+    void updateTrapPlaced(int player, int posx, int posy, TrapTypes trapType);
 
-    void updateWeaponPlacedOnIsland(int joueur, int posx, int posy, WeaponTypes weaponType);
+    void updateWeaponPlacedOnIsland(int player, int posx, int posy, WeaponTypes weaponType);
 
-    void updateShipCellDrowned(int joueur, int posx, int posy);
+    void updateShipCellDrowned(int player, int posx, int posy);
 
-    void updateNoMoreShips(int joueur);
+    void updateNoMoreShips(int player);
 }

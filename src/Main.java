7142029eb99cb327c986aca7bfda975475b3e622
ISNARD BASTIEN <bastien.isnard@etu.unistra.fr>
2025-12-20 14:06:59 +0100

    Plus qu'un seul main

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,13 @@
 import controller.GameController;
-import model.*;
-import model.traps.BlackHole;
-import model.traps.Tornado;
-import model.traps.Trap;
+import controller.bots.SmartBot;
+import model.Game;
+import view.GraphicalView;
 import view.TerminalView;
 
-import java.util.Arrays;
-
-public class Main{
-    public static void main(String[] args) {
-        /*
-        Game model = new Game();
-        GameController controller = new GameController(model);
-
-        TerminalView view = new TerminalView(model,controller);
-        view.run();
-
-        controller.setPlayerName("Bast");
-        controller.setGrid(10,false);
-
-        controller.placeTrapOnGrid(0,0,0,0);
-
-        controller.placeShipOnGrid(0,ShipTypes.Cruiser,3,0, Axis.VERTICAL);
-        controller.placeShipOnGrid(1,ShipTypes.Cruiser,3,0, Axis.VERTICAL);
-
-        controller.setWeapon(1, 1);
-        controller.shootOnGrid(1,0,3);
-
-        System.out.println(model.isTheGameFinished());
-
-        controller.setWeapon(0, 0);
-        controller.shootOnGrid(0,3,0);
-        controller.shootOnGrid(0,3,1);
-        controller.shootOnGrid(0,3,2);
-        controller.shootOnGrid(0,3,3);
-
-        System.out.println(model.isTheGameFinished());
-        */
+public class Main {
+    public static void main(String[] args){
+        Game game = new Game();
+        GameController gamecontroller = new GameController(game, new SmartBot());
+        GraphicalView graphicalView = new GraphicalView(gamecontroller, game);
     }
-
 }
